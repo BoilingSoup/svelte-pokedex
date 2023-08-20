fb9488b47ce7248f7bea0f89cfc4cd9caf379811
BoilingSoup <boilingsoup@protonmail.com>
2023-08-20 15:20:10 -0700

    dist

--- src/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { API } from "./constants";
-
-export async function fetchPoke(id: number) {
-  const result = await fetch(API + id);
-  return (await result.json()) as { sprites: { front_default: string } };
-}
