34b2574b8a80aba1ba193ef5012a7a66b8dc1bb1
BoilingSoup <boilingsoup@protonmail.com>
2023-08-20 14:58:34 -0700

    fin

---
+++ src/Pokedex.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { createQuery } from "@tanstack/svelte-query";
+  import { mainCss, screenCss } from "./styles";
+  import { fetchPoke } from "./helpers";
+
+  let started = false;
+  let pokeID: number;
+
+  $: query = createQuery({
+    queryKey: [pokeID],
+    queryFn: () => fetchPoke(pokeID),
+    enabled: started,
+  });
+
+  onMount(() => {
+    const handleSpacebar = (e: KeyboardEvent) => {
+      if (e.key !== " ") {
+        return;
+      }
+      started = true;
+      pokeID = Math.floor(Math.random() * 700 + 1);
+    };
+
+    window.addEventListener("keydown", handleSpacebar);
+    return () => window.removeEventListener("keydown", handleSpacebar);
+  });
+</script>
+
+<main class={mainCss}>
+  <section class={screenCss}>
+    {#if !started}
+      press SPACE to fetch a random pokemon
+    {:else if $query.isLoading}
+      <p>Loading...</p>
+    {:else if $query.isError}
+      <p>Error: {$query.error}</p>
+    {:else if $query.isSuccess}
+      <img
+        src={$query.data.sprites.front_default}
+        alt="img"
+        height="300"
+        width="300"
+      />
+    {/if}
+  </section>
+</main>
---
+++ src/styles.ts
@@ -0,0 +1,20 @@
+import { css } from "../styled-system/css";
+
+export const mainCss = css({
+  height: "full",
+  width: "full",
+  background: "red.400",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+export const screenCss = css({
+  height: "350px",
+  width: "350px",
+  rounded: "full",
+  background: "teal.500",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
